927602d30768b95e4d184f542eab80d205639c86
CITMark <86649048+CITMark@users.noreply.github.com>
2023-12-01 10:30:55 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "MXParallaxHeader",
-    platforms: [ .iOS(.v9)],
     products: [
         .library(
             name: "MXParallaxHeader",
